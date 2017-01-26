8d6a3466cf6b685e3f39f25d9b6be9431457c1fa
ved_m <ved@sigmoidanalytics.com>
2017-01-26 10:35:23 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require("http");
+
+http.createServer(function(request,response){
+
+	response.writeHead(200, {'Content-Type':'text/plain'});
+
+	response.end('Hello world\n');
+}).listen(8080);
+
+console.log('Server running on port 8080');
